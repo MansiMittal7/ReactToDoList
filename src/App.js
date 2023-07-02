a8e87a1f3f2dd36e1000827ee1364399957112f1
Mansi Mittal <mansi27mittal@gmail.com>
2023-07-03 02:45:42 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from 'react';
+import './App.css';
+import Data from './Data';
+
+export default function App() {
+
+  const [input, setinput] = useState("");
+  const [Items, setItems] = useState([]);
+
+  const addItem = (e) => {
+    // jo apan typ kar rhe hai usko searchbar me dikhana
+  setinput(e.target.value);   
+
+  };
+
+  const listOfItem = () => {
+  //added items ko array k from me store karna
+  setItems((oldItem) =>{
+    return [...oldItem,input];
+  });
+  //after addition of input to list clrear the search space
+   setinput("");
+  };
+  const deleteItem = (id) =>{
+    setItems((oldItems) => {
+      return oldItems.filter((arrElement, index) => {
+         return index!==id
+      })
+    })
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+  <div className="main_div">
+    <div className="center_div">
+     <br />
+     <h1>TO-DO LIST</h1>
+     <br />
+     <input type="text" placeholder="Add an Item" 
+     value={input}  //
+     onChange={addItem}/>
+     <button onClick={listOfItem}> + </button>
+     <ol>
+      {/* <li>{addItem}</li> */}
+      {
+      Items.map( (itemval, index) =>{
+       return <Data //inside data component
+       key={index}  //properties
+       id={index}
+       text={itemval}
+       onSelect = {deleteItem}
+       />;
+      })
+      }
+
+     </ol>
+     </div>
+  </div>
+  </>
+  )
 }
 
-export default App;
+
---
+++ src/Data.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Data(props) {
+  
+ 
+  return (
+    <>
+    <div className='todo_style'>
+    <i 
+    className="fa fa-times" 
+    aria-hidden="true"
+    onClick={() =>{
+       props.onSelect(props.id);
+    }}
+    />
+     <li>{props.text}</li>
+     </div>
+     </>
+  )
+}
